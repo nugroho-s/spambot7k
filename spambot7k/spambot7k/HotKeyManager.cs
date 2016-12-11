e87d931fef1bcb59779c80cf4eefcec42e607855
nugroho-s <massatriya@gmail.com>
2016-12-11 16:06:37 +0700

    add hotkey start

---
+++ spambot7k/spambot7k/HotKeyManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace spambot7k
+{
+    public static class HotKeyManager
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        public static void StartLogging()
+        {
+            bool stop = false;
+            while (!stop)
+            {
+                //sleeping for while, this will reduce load on cpu
+                Thread.Sleep(10);
+                for (Int32 i = 0; i < 255; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState == 1 || keyState == -32767)
+                    {
+                        if (i == 112)
+                            stop = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ spambot7k/spambot7k/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spambot7k
+{
+    class Program
+    {
+        // DLL libraries used to manage hotkeys
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        const int MYACTION_HOTKEY_ID = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tekan F1 untuk mulai spam");
+            HotKeyManager.StartLogging();
+            Console.WriteLine("Memulai spam");
+            return;
+        }
+    }
+}
